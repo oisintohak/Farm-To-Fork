f720b9d2b5a1bd95a2893b2f6094bba097179d0a
Oisín Tohak <oisintohak@gmail.com>
2021-08-03 19:22:25 +0530

    add allowed host in settings

--- farm_to_fork/settings.py
+++ farm_to_fork/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = ['farmtofork.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['farmtofork.herokuapp.com', 'localhost']
 
 
 # Application definition
