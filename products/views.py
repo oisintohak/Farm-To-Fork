af5d990508353e26fd92cf6b906443c6c070aaeb
Oisín Tohak <oisintohak@gmail.com>
2021-08-10 16:33:28 +0530

    modify product list view

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.http.response import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic.detail import DetailView
 from django.contrib import messages
+from django.shortcuts import get_list_or_404, get_object_or_404
 from django.views.generic import ListView
 from extra_views import (
     CreateWithInlinesView,
@@ -10,6 +11,7 @@ from extra_views import (
     InlineFormSetFactory
 )
 from .models import ProductVariant, Product
+from accounts.models import UserModel
 from .forms import ProductVariantFormHelper
 
 
@@ -38,7 +40,8 @@ class ProductList(ListView):
             product_list[index] = {
                 'product': product,
                 'variants': ProductVariant.objects.filter(
-                    product=product)
+                    product=product),
+                'owner': get_object_or_404(UserModel, pk=product.created_by.id)
             }
 
         context['product_list'] = product_list
