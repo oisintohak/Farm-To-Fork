ee002b3608d30f007fa81fdde78772ed48b421c3
Oisín Tohak <oisintohak@gmail.com>
2021-08-23 13:27:21 +0530

    add distance data to checkout items

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,9 @@
-from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
 from django.views.generic.base import TemplateView
 from django.conf import settings
-from multi_form_view import MultiModelFormView
+
+
 from profiles.forms import AddressForm
 from profiles.models import UserProfile
 from .forms import OrderForm
@@ -11,6 +11,8 @@ from .mixins import EmptyCartMixin
 from cart.contexts import cart_contents
 from .models import Order
 
+from multi_form_view import MultiModelFormView
+from geopy import distance
 import stripe
 
 
@@ -33,9 +35,12 @@ class Checkout(EmptyCartMixin, MultiModelFormView):
             initial['order_form']['first_name'] = profile.first_name
             initial['order_form']['last_name'] = profile.last_name
             initial['order_form']['phone_number'] = profile.phone_number
-            initial['address_form']['street_address1'] = profile.address.street_address1
-            initial['address_form']['street_address2'] = profile.address.street_address2
-            initial['address_form']['town_or_city'] = profile.address.town_or_city
+            initial['address_form']['street_address1'] = (
+                profile.address.street_address1)
+            initial['address_form']['street_address2'] = (
+                profile.address.street_address2)
+            initial['address_form']['town_or_city'] = (
+                profile.address.town_or_city)
             initial['address_form']['county'] = profile.address.county
             initial['address_form']['postcode'] = profile.address.postcode
             initial['address_form']['country'] = profile.address.country
@@ -43,12 +48,11 @@ class Checkout(EmptyCartMixin, MultiModelFormView):
 
     def forms_valid(self, all_forms):
         order = all_forms['order_form'].save(commit=False)
-        print(type(order))
         order.address = all_forms['address_form'].save()
         order.save()
-        print('order')
-        print(order.order_number)
-        return redirect(reverse('payment', kwargs={'order_number': order.order_number}))
+        return redirect(
+            reverse('payment', kwargs={'order_number': order.order_number})
+        )
 
     def get_success_url(self):
         return 'payment'
@@ -61,20 +65,34 @@ class Payment(EmptyCartMixin, TemplateView):
     """
     template_name = 'checkout/payment.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get_cart_items_with_distance(self):
+        order = get_object_or_404(
+            Order, order_number=self.kwargs['order_number'])
         current_cart = cart_contents(self.request)
-        context['stripe_public_key'] = settings.STRIPE_PUBLIC_KEY
-        context['stripe_secret_key'] = settings.STRIPE_SECRET_KEY
+        order_location = order.address.location
+        for item in current_cart['cart_items']:
+            item['location'] = (
+                item['product'].product.created_by.profile.address.location)
+            item['distance'] = (
+                distance.distance(item['location'], order_location).km)
+        return current_cart
 
-        stripe.api_key = 'sk_test_51J3J6ySIpQvL8FY5gm76sDjVWukN4NnOxZVrpcOuaOuCd5JNhrbE3uqroK0NCC7VRSNi8ygdwNhW5Jvrq4R846WD00NOLZcbtp'
-        intent = stripe.PaymentIntent.create(
-            amount=int(round(current_cart['total']*100)),
-            currency=settings.STRIPE_CURRENCY,
-        )
-        context['client_secret'] = intent.client_secret
-
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
         context['order'] = get_object_or_404(
             Order, order_number=self.kwargs['order_number'])
-        # print(intent)
+        return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_cart = self.get_cart_items_with_distance()
+        context['stripe_public_key'] = settings.STRIPE_PUBLIC_KEY
+        context['stripe_secret_key'] = settings.STRIPE_SECRET_KEY
+        # stripe.api_key = settings.STRIPE_SECRET_KEY
+        # intent = stripe.PaymentIntent.create(
+        #     amount=int(round(current_cart['total']*100)),
+        #     currency=settings.STRIPE_CURRENCY,
+        # )
+        # context['client_secret'] = intent.client_secret
+        context['current_cart'] = current_cart
         return context
