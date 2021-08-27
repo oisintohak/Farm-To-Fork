59e9ca0231115d8b0f534bf4c4427a3426235b38
Oisín Tohak <oisintohak@gmail.com>
2021-08-27 17:02:10 +0530

    add product and farmer sliders to home page

--- home/views.py
+++ home/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from accounts.models import UserModel
+from profiles.models import UserProfile
+from products.models import Product, ProductVariant
 
 
-def index(request):
-    """
-    Render the home page
-    """
-    return render(request, 'home/index.html')
+class Home(TemplateView):
+    template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['farmers'] = UserProfile.objects.filter(
+            user__groups__name='Farmers')
+        products = Product.objects.all()
+        product_list = {}
+        for index, product in enumerate(products):
+            product_list[index] = {
+                'product': product,
+                'variants': ProductVariant.objects.filter(
+                    product=product),
+                'owner': get_object_or_404(UserModel, pk=product.created_by.id)
+            }
+
+        context['product_list'] = product_list
+        return context
