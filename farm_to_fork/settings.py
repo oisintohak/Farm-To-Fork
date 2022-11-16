9b1bd39aeb8383332ee4c66fe2632fde0118909c
oisintohak <oisintohak@gmail.com>
2022-11-16 16:07:01 +0000

    add settings for render deploy

--- farm_to_fork/settings.py
+++ farm_to_fork/settings.py
@@ -42,6 +42,11 @@ if 'HEROKU' not in os.environ:
 
 ALLOWED_HOSTS = ['farmtoforkheroku.herokuapp.com', '127.0.0.1']
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
